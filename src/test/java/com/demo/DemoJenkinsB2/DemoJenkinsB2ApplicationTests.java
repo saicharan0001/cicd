322855756cea14d1050e3a5854ec1b1032173700
saicharan <saicharan_b200970ec@nitc.ac.in>
2024-07-04 22:58:51 +0530

    hellos

---
+++ src/test/java/com/demo/DemoJenkinsB2/DemoJenkinsB2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.DemoJenkinsB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsB2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
