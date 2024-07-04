fa2c3a967d793e455c9861e6fc4071d7d244ddcf
saicharan <saicharan_b200970ec@nitc.ac.in>
2024-07-04 22:52:25 +0530

    firstcommit

---
+++ DemoJenkinsB2/src/main/java/com/demo/DemoJenkinsB2/DemoJenkinsB2Application.java
@@ -0,0 +1,13 @@
+package com.demo.DemoJenkinsB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsB2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsB2Application.class, args);
+	}
+
+}
